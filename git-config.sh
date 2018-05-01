1655c11322debced7ae21810372a41ffa5632368
Daniel Wennberg <daniel.wennberg@gmail.com>
2018-04-30 18:07:12 -0700

    Add gitconfig (who knew?)

--- git-config.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-git config --global user.name "Daniel Wennberg"
-git config --global user.email "daniel.wennberg@gmail.com"
-git config --global color.ui true
-git config --global credential.helper cache
-#git config --global push.default simple
