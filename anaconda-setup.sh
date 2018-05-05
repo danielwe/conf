5489c30b23ff8c362891d831a28976430deab37c
Daniel Wennberg <daniel.wennberg@gmail.com>
2018-05-05 13:18:25 -0700

    Include nbextensions in anaconda setup

--- anaconda-setup.sh
+++ anaconda-setup.sh
@@ -7,3 +7,6 @@ VERSION="5.1.0"
 wget -O "anaconda.sh" "https://repo.continuum.io/archive/Anaconda3-${VERSION}-Linux-x86_64.sh"
 bash "anaconda.sh"
 rm "anaconda.sh"
+
+pip install jupyter-contrib-nbextension
+jupyter contrib nbextension install --sys-prefix
