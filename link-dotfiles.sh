caa913bd2ebd1def64776c304d371d57e2aace98
Daniel Wennberg <daniel.wennberg@gmail.com>
2018-08-05 17:38:28 -0700

    Add support for dot folders

--- link-dotfiles.sh
+++ link-dotfiles.sh
@@ -8,3 +8,14 @@ do
     mkdir -p "$( dirname "${LINK_NAME}" )"
     ln -sn --backup=numbered "${TARGET}" "${LINK_NAME}"
 done
+
+# Support entire dot directories. Usage: a folder inside 'dotfolders' named
+# 'one.two.three' will be linked to by '~/.one/two/three'
+DOTDIR="${DIR}/dotfolders"
+for TARGET in "${DOTDIR}"/*
+do
+    LINK_NAME="${TARGET//\./\/}"
+    LINK_NAME="${HOME}/.${LINK_NAME/#${DOTDIR}\//}"
+    mkdir -p "$( dirname "${LINK_NAME}" )"
+    ln -sn --backup=numbered "${TARGET}" "${LINK_NAME}"
+done
