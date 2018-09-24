c4f9ea6cc3605a5bda84d2df90bdaecc644017bf
Daniel Wennberg <daniel.wennberg@gmail.com>
2018-09-24 12:29:35 -0700

    Adjust notebook conf: Black line length ++

--- notebook-theme.sh
+++ notebook-theme.sh
@@ -2,4 +2,4 @@
 
 # Set notebook theme, assuming jupyterthemes installed
 
-jt -t oceans16 -f roboto -nf robotosans -nfs 11 -tf merriserif -tfs 11 -cellw 841 -lineh 130 -vim
+jt -t oceans16 -f ubuntu -fs 11 -nf ptsans -nfs 11 -tf ptserif -tfs 11 -lineh 130 -vim
