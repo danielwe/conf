a4b06d689ab02894ab4af787b94f146efc8f7e1c
Daniel Wennberg <daniel.wennberg@gmail.com>
2018-12-10 12:08:06 -0800

    Use new notebook theme

--- notebook-theme.sh
+++ notebook-theme.sh
@@ -2,4 +2,4 @@
 
 # Set notebook theme, assuming jupyterthemes installed
 
-jt -t oceans16 -f ubuntu -fs 11 -nf ptsans -nfs 10 -tf ptserif -tfs 11 -lineh 100 -cellw 796 -vim
+jt -t grade3 -f ubuntu -fs 14 -nf ptsans -nfs 10 -tf ptsans -tfs 16 -lineh 100 -cellw 980 -vim -T
