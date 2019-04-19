ada2664c916e9b2d913b887dbe9dda5d84d5af53
Daniel Wennberg <daniel.wennberg@gmail.com>
2019-04-18 20:12:00 -0700

    Finally, the proper GPU solution!

--- cuda-driver.sh
+++ cuda-driver.sh
@@ -2,45 +2,76 @@
 
 # Prepare the nvidia driver for use with CUDA installed from e.g. anaconda:
 
-# PREFERRED METHOD
+# WITH MANUAL TOGGLING, NO NVIDIA-PRIME
 
-# With bbswitch (no nvidia-prime).
-BLACKLIST_CONF=/etc/modprobe.d/blacklist-nvidia.conf
-BBSWITCH_LOAD=/etc/modules-load.d/bbswitch.conf
-BBSWITCH_CONF=/etc/modprobe.d/bbswitch.conf
+# Full, automatically upgraded drivers, with CUDA
+sudo apt install cuda  # or just cuda-drivers
 
 # Pinned, headless driver
 #VERSION=410  # Check preferred repo for latest version
 #sudo apt install nvidia-headless-$VERSION nvidia-utils-$VERSION libnvidia-cfg1-$VERSION
 
-# Full, automatically upgraded drivers, with CUDA
-sudo apt install cuda  # or just cuda-drivers
+BLACKLIST_CONF=/etc/modprobe.d/blacklist-nvidia.conf
 
-sudo systemctl disable nvidia-fallback.service
 echo "blacklist nvidia" | sudo tee "$BLACKLIST_CONF"
-echo "blacklist nvidia-uvm" | sudo tee -a "$BLACKLIST_CONF"
-echo "blacklist nvidia-drm" | sudo tee -a "$BLACKLIST_CONF"
-echo "blacklist nvidia-modeset" | sudo tee -a "$BLACKLIST_CONF"
+echo "blacklist nvidia_uvm" | sudo tee -a "$BLACKLIST_CONF"
+echo "blacklist nvidia_modeset" | sudo tee -a "$BLACKLIST_CONF"
+echo "blacklist nvidia_drm" | sudo tee -a "$BLACKLIST_CONF"
 
-sudo apt install bbswitch-dkms
-echo "bbswitch" | sudo tee "$BBSWITCH_LOAD"
-echo "options bbswitch load_state=0" | sudo tee "$BBSWITCH_CONF"
+# PREFERRED: automatic power management by the kernel. With recent kernels this is
+# sufficient for the GPU to be powered down when drivers are unloaded.
+
+# Make sure that GPU is completely off on boot to avoid driver loading by anyone
+# (kernel, xorg, gnome, nvidia-fallback.service, ...)
+OFF_CONF=/etc/tmpfiles.d/nvidia-off.conf
+echo "w /sys/bus/pci/devices/0000:01:00.0/remove - - - - 1" | sudo tee "$OFF_CONF"
 
 sudo update-initramfs -u
 
-sudo sed -i 's/\(GRUB_CMDLINE_LINUX_DEFAULT=".*\)"$/\1 nogpumanager"/' /etc/default/grub
-# Some hardware requires the following kernel parameter to work with bbswitch:
-sudo sed -i 's/\(GRUB_CMDLINE_LINUX_DEFAULT=".*\)"$/\1 acpi_rev_override"/' /etc/default/grub
-sudo update-grub
 # Restart.
+
 # To turn on nvidia GPU:
-# sudo tee /proc/acpi/bbswitch <<< ON
-# sudo modprobe nvidia
-# sudo nvidia-smi -pm 1  # Optional, enables persistence mode
+#GPUID="0000:01:00.0"
+#if [[ ! -e "/sys/bus/pci/devices/$GPUID" ]]
+#then
+#    sudo tee /sys/bus/pci/rescan <<< 1
+#fi
+#sudo tee "/sys/bus/pci/devices/$GPUID/power/control" <<< auto  # Ensure automatic pm
+#sudo modprobe nvidia && sudo nvidia-smi -pm 1  # Optional, enables persistence mode
 # To turn off nvidia GPU:
-# sudo nvidia-smi -pm 0
-# sudo modprobe -r nvidia_drm nvidia_uvm
-# sudo tee /proc/acpi/bbswitch <<< OFF
+#sudo modprobe -r nvidia_drm nvidia_modeset nvidia_uvm
+#sudo nvidia-smi -pm 0 && sudo modprobe -r nvidia
+#sudo tee "/sys/bus/pci/devices/$GPUID/power/control" <<< auto  # Ensure automatic pm
+
+# Power management using bbswitch. Note that the GPU is correctly powered off with
+# recent kernels, even if bbswitch complains to syslogs that it's not. (Check power
+# consumption in powertop or similar to confirm).
+#
+##sudo systemctl disable nvidia-fallback.service  # No longer present for recent drivers
+#
+#BBSWITCH_LOAD=/etc/modules-load.d/bbswitch.conf
+#BBSWITCH_CONF=/etc/modprobe.d/bbswitch.conf
+#
+#sudo apt install bbswitch-dkms
+#echo "bbswitch" | sudo tee "$BBSWITCH_LOAD"
+#echo "options bbswitch load_state=0" | sudo tee "$BBSWITCH_CONF"
+#
+#sudo update-initramfs -u
+#
+#sudo sed -i 's/\(GRUB_CMDLINE_LINUX_DEFAULT=".*\)"$/\1 nogpumanager"/' /etc/default/grub
+## Some hardware requires the following kernel parameter to work with bbswitch:
+#sudo sed -i 's/\(GRUB_CMDLINE_LINUX_DEFAULT=".*\)"$/\1 acpi_rev_override"/' /etc/default/grub
+#sudo update-grub
+#
+# Restart.
+#
+# To turn on nvidia GPU:
+#sudo tee /proc/acpi/bbswitch <<< ON
+#sudo modprobe nvidia && sudo nvidia-smi -pm 1  # Optional, enables persistence mode
+# To turn off nvidia GPU:
+#sudo modprobe -r nvidia_drm nvidia_modeset nvidia_uvm
+#sudo nvidia-smi -pm 0 && sudo modprobe -r nvidia
+#sudo tee /proc/acpi/bbswitch <<< OFF
 
 
 # OTHER METHODS:
@@ -54,7 +85,7 @@ sudo update-grub
 # Restart.
 # 
 # Before using CUDA, execute:
-# sudo modprobe nvidia
+#sudo modprobe nvidia
 #
 # NOTE: driver version 410 from the CUDA repos contains a more insistent persistence
 # daemon that makes the system unstable when configured as above, especially when waking
@@ -62,21 +93,21 @@ sudo update-grub
 
 # Older versions:
 
-# BASHRC="${HOME}/.bashrc"
+#BASHRC="${HOME}/.bashrc"
 
 # add nvidia driver to linker path if not the selected graphics card
-# echo >> ${BASHRC}
-# echo '# Add nvidia binaries and libraries to path if offline, for CUDA' >> ${BASHRC}
-# echo 'if [ "'"$( prime-select query )"'" == "'"intel"'" ]' >> ${BASHRC}
-# echo 'then' >> ${BASHRC}
-# echo '    nvdir=( /usr/lib/nvidia-??? )' >> ${BASHRC}
-# echo '    if [ ${#nvdir[@]} -ge 0 ]' >> ${BASHRC}
-# echo '    then' >> ${BASHRC}
-# echo '        export PATH="${nvdir[-1]}/bin${PATH:+:${PATH}}"' >> ${BASHRC}
-# echo '        export LD_LIBRARY_PATH="${nvdir[-1]}${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}"' >> ${BASHRC}
-# echo '    fi' >> ${BASHRC}
-# echo '    unset nvdir' >> ${BASHRC}
-# echo 'fi 2>/dev/null' >> ${BASHRC}
+#echo >> ${BASHRC}
+#echo '# Add nvidia binaries and libraries to path if offline, for CUDA' >> ${BASHRC}
+#echo 'if [ "'"$( prime-select query )"'" == "'"intel"'" ]' >> ${BASHRC}
+#echo 'then' >> ${BASHRC}
+#echo '    nvdir=( /usr/lib/nvidia-??? )' >> ${BASHRC}
+#echo '    if [ ${#nvdir[@]} -ge 0 ]' >> ${BASHRC}
+#echo '    then' >> ${BASHRC}
+#echo '        export PATH="${nvdir[-1]}/bin${PATH:+:${PATH}}"' >> ${BASHRC}
+#echo '        export LD_LIBRARY_PATH="${nvdir[-1]}${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}"' >> ${BASHRC}
+#echo '    fi' >> ${BASHRC}
+#echo '    unset nvdir' >> ${BASHRC}
+#echo 'fi 2>/dev/null' >> ${BASHRC}
 
 
 # Manual CUDA install: lots of hoops to jump through, but in the end you want
@@ -85,11 +116,11 @@ sudo update-grub
 # add cuda binaries to path
 #if [ -d "/usr/local/cuda" ] ; then
 #    PATH="/usr/local/cuda/bin:$PATH"
-
-    # manually add cuda libraries to linker path
-    #export CUDA_HOME="/usr/local/cuda"
-    #export LD_LIBRARY_PATH="$CUDA_HOME/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}"
-    #export LD_LIBRARY_PATH="$CUDA_HOME/nvvm/lib64:${LD_LIBRARY_PATH}"
-    #export LD_LIBRARY_PATH="$CUDA_HOME/extras/CUPTI/lib64:${LD_LIBRARY_PATH}"
-    #export LD_LIBRARY_PATH="$CUDA_HOME/extras/Debugger/lib64:${LD_LIBRARY_PATH}"
+#
+#    # manually add cuda libraries to linker path
+#    export CUDA_HOME="/usr/local/cuda"
+#    export LD_LIBRARY_PATH="$CUDA_HOME/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}"
+#    export LD_LIBRARY_PATH="$CUDA_HOME/nvvm/lib64:${LD_LIBRARY_PATH}"
+#    export LD_LIBRARY_PATH="$CUDA_HOME/extras/CUPTI/lib64:${LD_LIBRARY_PATH}"
+#    export LD_LIBRARY_PATH="$CUDA_HOME/extras/Debugger/lib64:${LD_LIBRARY_PATH}"
 #fi
