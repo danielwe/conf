b8dbec665d7cd6703eb20047eac0b3bca1329024
Daniel Wennberg <daniel.wennberg@gmail.com>
2020-03-09 18:27:42 -0700

    Bump julia version

--- julia-upgrade.sh
+++ julia-upgrade.sh
@@ -2,8 +2,8 @@
 
 # Upgrade julia
 
-MAJOR_VERSION="1.2"
-FULL_VERSION="1.2.0"
+MAJOR_VERSION="1.3"
+FULL_VERSION="1.3.1"
 ARCHIVE="julia-${FULL_VERSION}-linux-x86_64.tar.gz"
 
 # Move to installation directory
