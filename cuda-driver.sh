779fcf44553963aa677708bb814767d0d0fbbff8
Daniel Wennberg <daniel.wennberg@gmail.com>
2020-09-24 17:14:23 -0700

    Power manage graphics USB and audio too

--- cuda-driver.sh
+++ cuda-driver.sh
@@ -15,10 +15,22 @@ echo \
 options nvidia "NVreg_DynamicPowerManagement=0x02"' \
   | sudo tee "/etc/modprobe.d/nvidia.conf"
 
-# Hooks to ensure consistent power control states on driver load/unload. See
+# Hooks to ensure consistent power control states on driver load/unload, and disable USB
+# and audio for kernels < 5.5. See
 # https://download.nvidia.com/XFree86/Linux-x86_64/450.51/README/dynamicpowermanagement.html.
 echo \
-'# Enable runtime PM for NVIDIA VGA/3D controller devices on driver bind
+'# TODO: Remove the first three rules once kernel 5.5 lands
+
+# Remove NVIDIA USB xHCI Host Controller devices, if present
+ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{remove}="1"
+
+# Remove NVIDIA USB Type-C UCSI devices, if present
+ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{remove}="1"
+
+# Remove NVIDIA Audio devices, if present. NOTE: will disable audio over HDMI.
+ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{remove}="1"
+
+# Enable runtime PM for NVIDIA VGA/3D controller devices on driver bind
 ACTION=="bind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030000", TEST=="power/control", ATTR{power/control}="auto"
 ACTION=="bind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030200", TEST=="power/control", ATTR{power/control}="auto"
 
